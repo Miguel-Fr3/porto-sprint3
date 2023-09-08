58a8e4e11b5fc408ddd6dfe714f00cc3f50c1073
Miguel-Fr3 <rm99997@fiap.com.br>
2023-09-08 12:21:04 -0300

    importando logo

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,11 +1,21 @@
+import logo from '../assets/Logo.png'
 export default function Cabecalho() {
     return(
         <>
         <header>
-            <img src="" alt="" />
-            <h1>porto</h1>
-            <h1>porto</h1>
-            <h1>porto</h1>
+            <div>
+                <img src={logo} alt="Logo da porto" />
+            </div>
+            <div>
+            <nav>
+            <h1>Ajuda</h1>
+            <h1>Modals</h1>
+            <h1>Sobre nós</h1>
+            </nav>
+            </div>
+            <div>
+
+            </div>
             </header>
         </>
     )
