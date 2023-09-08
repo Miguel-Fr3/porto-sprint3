ec9949e2d32f1aa662e05ca6e9a5da4f01cc182f
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-09-08 20:34:51 -0300

    Criando divs

--- src/routes/Ajuda/index.jsx
+++ src/routes/Ajuda/index.jsx
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import CabecalhoA from "../../components/CabecalhoA"
+import { faContactBook } from "@fortawesome/free-solid-svg-icons"
 export default function Ajuda (){
     return(
         <main className="ajuda">
@@ -6,6 +8,16 @@ export default function Ajuda (){
             <div className="help">
             <label htmlFor="ajuda">Preencha seus dados de acesso para continuar.</label>
             <input type="text" id="ajuda" placeholder="Pesquise o que você precisa aqui!" />
+            </div>
+            <div className="superior">
+                <div><h1>Solicitar Socorro</h1></div>
+                <div><FontAwesomeIcon/><h1>Contatos</h1></div>
+                <div><h1>Editar dados</h1></div>
+            </div>
+            <div className="inferior">
+                <div><h1>App porto</h1></div>
+                <div><h1>Pagamentos</h1></div>
+                <div><h1>Benefícios</h1></div>
             </div>
         </main>
     )
