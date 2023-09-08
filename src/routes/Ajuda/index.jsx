4d4a3f87606908258c3adee2253f36f72519173e
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-09-08 20:25:53 -0300

    Iniciando pagina de ajuda

--- src/routes/Ajuda/index.jsx
+++ src/routes/Ajuda/index.jsx
@@ -3,7 +3,10 @@ export default function Ajuda (){
     return(
         <main className="ajuda">
             <CabecalhoA/>
-            <h1>Ajuda</h1>
+            <div className="help">
+            <label htmlFor="ajuda">Preencha seus dados de acesso para continuar.</label>
+            <input type="text" id="ajuda" placeholder="Pesquise o que você precisa aqui!" />
+            </div>
         </main>
     )
 }
