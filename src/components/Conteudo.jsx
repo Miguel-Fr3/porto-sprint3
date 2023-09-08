2817f0d2d85f7a7de7f7c5fc51f740257cce6545
Miguel-Fr3 <rm99997@fiap.com.br>
2023-09-08 13:24:02 -0300

    Estilizando conteudo

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,11 +1,14 @@
 import img3 from '../assets/img3.png'
+import img8 from '../assets/img8.png'
 export default function Conteudo (){
     return(
         <>
         <section>
         <div className="superior">
-            <p>Guincho 24 horas
-                Guinchos ilimitados, sem limite de quilometragem.
+        <p>
+            <h1>Guincho 24 horas</h1>
+            
+                Guinchos ilimitados, sem limite de quilometragem. <br />
                 Reparos do carro
                 Reparo rápido e Supermartelinho de Ouro.</p>
             <img src={img3} alt="" className='imgC'/>
@@ -13,7 +16,10 @@ export default function Conteudo (){
         </section>
         <section>
         <div className="inferior">
-
+        <p>
+            <h1>Reparos do carro</h1>
+            Reparo rápido e Supermartelinho de Ouro.</p>
+            <img src={img8} alt="" className='imgC'/>
         </div>
         </section>
         </>
