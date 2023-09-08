71842371f0fd24467ecc2f61f17ded4401a1153f
Luigi138 <nimapi20041012@gmail.com>
2023-09-08 17:30:00 -0300

    Estilizando o formulario

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,15 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CabecalhoD from "../../components/CabecalhoD"
+import { faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 export default function Login (){
     return(
         <main className="login">
             <CabecalhoD/>
+            <div className="form">
             <form className="formulario">
-                <label htmlFor="Cpf"></label>
+            <FontAwesomeIcon icon={faArrowLeft} className='Flecha' />
+                <h1>Acessar Conta</h1>
+                <label htmlFor="Cpf">Preencha seus dados de acesso para continuar.</label>
                 <input type="text" placeholder="CPF ou CNPJ"/>
                 <div className="register">
                     <input type="submit" value="Enviar"/>
                 </div>
             </form>
+            </div>
         </main>
     )
 }
