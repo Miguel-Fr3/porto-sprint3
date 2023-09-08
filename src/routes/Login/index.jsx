a60de4a1abb976569aba5ecd13d6d8db824b0140
Luigi138 <nimapi20041012@gmail.com>
2023-09-08 17:43:36 -0300

    Finalizando estilização do login

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,19 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CabecalhoD from "../../components/CabecalhoD"
 import { faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
 export default function Login (){
     return(
         <main className="login">
             <CabecalhoD/>
             <div className="form">
             <form className="formulario">
-            <FontAwesomeIcon icon={faArrowLeft} className='Flecha' />
+            <Link to="/"><FontAwesomeIcon icon={faArrowLeft} className='flecha' /></Link>
                 <h1>Acessar Conta</h1>
                 <label htmlFor="Cpf">Preencha seus dados de acesso para continuar.</label>
                 <input type="text" placeholder="CPF ou CNPJ"/>
                 <div className="register">
                     <input type="submit" value="Enviar"/>
                 </div>
+                <div className="check">
+                <input type="checkbox" />
+                <label htmlFor="Check">Lembre de mim</label>
+                </div>
+                <Link to="/cadastro">Sou novo aqui</Link>
             </form>
             </div>
         </main>
