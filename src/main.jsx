98ea97d71a9fdcffe8fa478f8eec3c80d1cedaa6
patrickjaguski <rm551521@fiap.com.br>
2023-09-08 14:09:18 -0300

    Importando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Ajuda from './routes/Ajuda/index.jsx'
+import Error from './routes/Error/index.jsx'
+import Login from './routes/Login/index.jsx'
+import Modals from './routes/Modals/index.jsx'
+import Sobre from './routes/Sobre/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
