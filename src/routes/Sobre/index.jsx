b2befd89189ae5ac4f7dacf829cf46bb09e7bbd7
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-09-09 08:30:27 -0300

    iniciando o sobre

--- src/routes/Sobre/index.jsx
+++ src/routes/Sobre/index.jsx
@@ -3,7 +3,22 @@ export default function Sobre (){
     return(
         <main className="sobre">
             <CabecalhoS/>
-            <h1>Sobre</h1>
+            <div>
+                <h1 className="propósito">Nosso propósito é</h1>
+                <h1 className="porto">Ser um porto seguro para as pessoas</h1>
+            </div>
+            <div>
+                <h1></h1>
+                <h2>rm99977 - Alberto Seiji
+
+                rm551997 - Matheus Rodrigues
+
+                rm99997 - Miguel Fernandes
+
+                rm551521 - Patrick Jaguski
+
+                rm552213 - Ye</h2>
+                </div>
         </main>
     )
 }
