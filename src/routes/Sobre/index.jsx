7576bba75cff422838f8fb1b036afb2395d644f8
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-09-09 09:10:45 -0300

    finalizando css do sobre

--- src/routes/Sobre/index.jsx
+++ src/routes/Sobre/index.jsx
@@ -11,15 +11,18 @@ export default function Sobre (){
             <div className="inferior">
             <div className="colab">
                 <h1>Colaboradores</h1>
-                <h2>rm99977 - Alberto Seiji
+                <h2>rm99977 - Alberto Seiji <br />
 
-                rm551997 - Matheus Rodrigues
+                rm551997 - Matheus Rodrigues <br />
 
-                rm99997 - Miguel Fernandes
+                rm99997 - Miguel Fernandes <br />
 
-                rm551521 - Patrick Jaguski
+                rm551521 - Patrick Jaguski <br />
 
-                rm552213 - Ye</h2>
+                rm552213 - Ye</h2> <br />
+                <nav>
+                <a href="https://github.com/Miguel-Fr3/porto-sprint3" target="_blank">Saiba mais</a>
+                </nav>
                 </div>
                 <div className="historia">
                     <img src={img0} alt="Fachada porto " />
@@ -28,8 +31,8 @@ export default function Sobre (){
                     Em 1945, a Porto é fundada, em São Paulo/SP, contando com cerca de 50 funcionários.
                     </h2>
 
-                <div>
-                    <button>Conheça mais</button>
+                <div className="botao">
+                    <a href="https://www.portoseguro.com.br/institucional/a-porto-seguro/historia" target="_blank">Conheça mais</a>
                 </div>
                 </div>
                 </div>
