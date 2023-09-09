2af868989f1cc946a1b4400742cc72ef89ed6148
matheusrcdrigues <matheusrcdrigues@gmail.com>
2023-09-09 08:35:53 -0300

    Terminando o html da pagina

--- src/routes/Sobre/index.jsx
+++ src/routes/Sobre/index.jsx
@@ -1,14 +1,15 @@
 import CabecalhoS from "../../components/CabecalhoS"
+import img0 from "../../assets/img0.png"
 export default function Sobre (){
     return(
         <main className="sobre">
             <CabecalhoS/>
             <div>
-                <h1 className="propósito">Nosso propósito é</h1>
+                <h1 className="proposito">Nosso propósito é</h1>
                 <h1 className="porto">Ser um porto seguro para as pessoas</h1>
             </div>
-            <div>
-                <h1></h1>
+            <div className="colab">
+                <h1>Colaboradores</h1>
                 <h2>rm99977 - Alberto Seiji
 
                 rm551997 - Matheus Rodrigues
@@ -18,6 +19,9 @@ export default function Sobre (){
                 rm551521 - Patrick Jaguski
 
                 rm552213 - Ye</h2>
+                </div>
+                <div className="historia">
+                    <img src={img0} alt="Fachada porto " />
                 </div>
         </main>
     )
