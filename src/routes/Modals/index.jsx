65eef181b9fafcfbeb779e618035b187fe019155
AlbertoSeiji <alberto.assatoalberto@gmail.com>
2023-09-10 22:32:36 -0300

    Implementando imagens

--- src/routes/Modals/index.jsx
+++ src/routes/Modals/index.jsx
@@ -12,42 +12,42 @@ export default function Modals (){
             <CabecalhoM/>
             <section className="conteudo">
                 <div className="modals">
-                    <img src={img5} alt="" />
+                    <img src={img5} alt="Guincho Leve" />
                     <h1>Guincho Leve</h1>
                     <p>
                         Utilizado para veículos de pequeno porte, como carros de passeio e motocicletas, em casos de pane mecânica, elétrica, pneu furado, entre outros.
                     </p>
                 </div>
                 <div className="modals">
-                    <img src="" alt="" />
+                    <img src={img4} alt="Guincho Pesado" />
                     <h1>Guincho Pesado</h1>
                     <p>
                     Destinado a veículos de maior porte, como caminhões e ônibus, utilizado em casos de panes mecânicas, acidentes ou remoção do veículo.
                     </p>
                 </div>
                 <div className="modals">
-                    <img src="" alt="" />
+                    <img src={img6} alt="Guincho Plataforma" />
                     <h1>Guincho Plataforma</h1>
                     <p>
                     Possui uma plataforma elevatória para transportar veículos com rodas travadas, danificados em acidentes ou quando não é possível rebocar o veículo normalmente.
                     </p>
                 </div>
                 <div className="modals">
-                    <img src="" alt="" />
+                    <img src={img10} alt="Guincho para Veículos Especiais" />
                     <h1>Guincho para Veículos Especiais</h1>
                     <p>
                     Projetado para transportar veículos especiais, como trailers, motorhomes e embarcações, levando em consideração suas características e dimensões únicas.
                     </p>
                 </div>
                 <div className="modals">
-                    <img src="" alt="" />
+                    <img src={img1} alt="Guincho 24 Horas" />
                     <h1>Guincho 24 Horas</h1>
                     <p>
                     Serviço disponível 24 horas por dia, todos os dias da semana, garantindo assistência em casos de emergência a qualquer momento.
                     </p>
                 </div>
                 <div className="modals">
-                    <img src="" alt="" />
+                    <img src={img9} alt="Guincho para Veículos 4x4" />
                     <h1>Guincho para Veículos 4x4</h1>
                     <p>
                     Especializado em reboques de veículos com tração nas quatro rodas, adequado para situações off-road e terrenos difíceis.
@@ -55,7 +55,7 @@ export default function Modals (){
                     </p>
                 </div>
                 <div className="modals">
-                    <img src="" alt="" />
+                    <img src={img2} alt="Reboque para Concessionária" />
                     <h1>Reboque para Concessionária</h1>
                     <p>
                     Serviço direcionado para levar o veículo diretamente a uma concessionária para reparo ou manutenção.
