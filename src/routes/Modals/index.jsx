29fc900fe9f134e0622a1d5d598264ee6542a7b6
AlbertoSeiji <alberto.assatoalberto@gmail.com>
2023-09-10 22:24:40 -0300

    implementando textos

--- src/routes/Modals/index.jsx
+++ src/routes/Modals/index.jsx
@@ -3,7 +3,59 @@ export default function Modals (){
     return(
         <main className="modal">
             <CabecalhoM/>
-            <section className="conteudo"></section>
+            <section className="conteudo">
+                <div className="modals">
+                    <img src="" alt="" />
+                    <h1>Guincho Leve</h1>
+                    <p>
+                        Utilizado para veículos de pequeno porte, como carros de passeio e motocicletas, em casos de pane mecânica, elétrica, pneu furado, entre outros.
+                    </p>
+                </div>
+                <div className="modals">
+                    <img src="" alt="" />
+                    <h1>Guincho Pesado</h1>
+                    <p>
+                    Destinado a veículos de maior porte, como caminhões e ônibus, utilizado em casos de panes mecânicas, acidentes ou remoção do veículo.
+                    </p>
+                </div>
+                <div className="modals">
+                    <img src="" alt="" />
+                    <h1>Guincho Plataforma</h1>
+                    <p>
+                    Possui uma plataforma elevatória para transportar veículos com rodas travadas, danificados em acidentes ou quando não é possível rebocar o veículo normalmente.
+                    </p>
+                </div>
+                <div className="modals">
+                    <img src="" alt="" />
+                    <h1>Guincho para Veículos Especiais</h1>
+                    <p>
+                    Projetado para transportar veículos especiais, como trailers, motorhomes e embarcações, levando em consideração suas características e dimensões únicas.
+                    </p>
+                </div>
+                <div className="modals">
+                    <img src="" alt="" />
+                    <h1>Guincho 24 Horas</h1>
+                    <p>
+                    Serviço disponível 24 horas por dia, todos os dias da semana, garantindo assistência em casos de emergência a qualquer momento.
+                    </p>
+                </div>
+                <div className="modals">
+                    <img src="" alt="" />
+                    <h1>Guincho para Veículos 4x4</h1>
+                    <p>
+                    Especializado em reboques de veículos com tração nas quatro rodas, adequado para situações off-road e terrenos difíceis.
+
+                    </p>
+                </div>
+                <div className="modals">
+                    <img src="" alt="" />
+                    <h1>Reboque para Concessionária</h1>
+                    <p>
+                    Serviço direcionado para levar o veículo diretamente a uma concessionária para reparo ou manutenção.
+
+                    </p>
+                </div>
+            </section>
         </main>
     )
 }
