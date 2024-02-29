4dca759f91ce495e86b611288042aa09ad9bee26
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2024-02-29 11:38:26 -0300

    minor fix

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
           <div>
             <h1>Guincho 24 horas</h1>
             <p>
-              Guinchos ilimitados, sem limite de quilometragem. <br />
+               Guinchos ilimitados, sem limite de quilometragem. <br />
               Reparos do carro Reparo rápido e Supermartelinho de Ouro.
             </p>
           </div>
